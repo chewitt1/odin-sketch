5c6c1d8df5d127c4da720001c8bf734dea65dc28
Carrima Hewitt <chewitt1@ufl.edu>
2023-05-16 23:43:50 -0400

    Update Tooltips Task buttons not working

--- test.js
+++ test.js
@@ -25,27 +25,58 @@ function Row(){
     return row;
 }
 
-/*MenuBar Functions */
+/*Tooltip Functions */
 function setToolTip(e){
     let val = this.innerHTML;
     let tooltip = document.getElementById("tooltip");
-    if(val == "File"){
-        tooltip.innerHTML = "Select File menu item";
+    let mainClass = this.classList[0];
+    if(mainClass == "menu-item"){
+        if(val == "File"){
+            tooltip.innerHTML = "Select File menu item";
+        }
+        else if(val == "Edit"){
+            tooltip.innerHTML = "Select Edit menu item";
+        }
+        else if(val == "View"){
+            tooltip.innerHTML = "Select View menu item";
+        }
+        else if(val == "Image"){
+            tooltip.innerHTML = "Select Image menu item";
+        }
+        else if(val == "Options"){
+            tooltip.innerHTML = "Select Options menu item";
+        }
+        else if(val == "Help"){
+            tooltip.innerHTML = "Select Help menu item";
+        }
     }
-    else if(val == "Edit"){
-        tooltip.innerHTML = "Select Edit menu item";
+    else if(mainClass == "tool-button1"){
+        tooltip.innerHTML = "Set erase mode";
     }
-    else if(val == "View"){
-        tooltip.innerHTML = "Select View menu item";
+    else if(mainClass == "tool-button2"){
+        tooltip.innerHTML = "Set draw mode";
     }
-    else if(val == "Image"){
-        tooltip.innerHTML = "Select Image menu item";
+    else if(mainClass = "grid-button"){
+        if(val == "NEW GRID"){
+            tooltip.innerHTML = "Create a new grid";
+        }
+        else if(val == "RESET"){
+            tooltip.innerHTML = "Reset current grid";
+        }
     }
-    else if(val == "Options"){
-        tooltip.innerHTML = "Select Options menu item";
+    else if(mainClass = "task-button"){
+        if(val == "-"){
+            tooltip.innerHTML = "Minimize Odin Sketch window";
+        }
+        else if(val == "[ ]"){
+            tooltip.innerHTML = "Remove Windows 95 background";
+        }
+        else if(val == "x"){
+            tooltip.innerHTML = "Close Odin Sketch window";
+        }
     }
-    else if(val == "Help"){
-        tooltip.innerHTML = "Select Help menu item";
+    else{
+        console.log(mainClass);
     }
 }
 
@@ -58,3 +89,8 @@ let menuItems = document.querySelectorAll(".menu-item");
 
 menuItems.forEach(item => item.addEventListener("mouseover", setToolTip));
 menuItems.forEach(item => item.addEventListener("mouseleave", resetToolTip));
+
+let gridButtons = document.querySelectorAll("button");
+
+gridButtons.forEach(butt => butt.addEventListener("mouseover", setToolTip));
+gridButtons.forEach(butt => butt.addEventListener("mouseleave", resetToolTip));
