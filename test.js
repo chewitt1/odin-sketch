dcd6851d6c7e7fcf1af650ac06b1d271a052a083
Carrima Hewitt <chewitt1@ufl.edu>
2023-05-16 23:29:29 -0400

    Add Tooltips

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 let rows = 27;
+
+/*Create Grid Functions*/
 let canvas = document.querySelector("#canvas");
 
 for(let i = 0; i < rows; i++){
@@ -22,3 +24,37 @@ function Row(){
     }
     return row;
 }
+
+/*MenuBar Functions */
+function setToolTip(e){
+    let val = this.innerHTML;
+    let tooltip = document.getElementById("tooltip");
+    if(val == "File"){
+        tooltip.innerHTML = "Select File menu item";
+    }
+    else if(val == "Edit"){
+        tooltip.innerHTML = "Select Edit menu item";
+    }
+    else if(val == "View"){
+        tooltip.innerHTML = "Select View menu item";
+    }
+    else if(val == "Image"){
+        tooltip.innerHTML = "Select Image menu item";
+    }
+    else if(val == "Options"){
+        tooltip.innerHTML = "Select Options menu item";
+    }
+    else if(val == "Help"){
+        tooltip.innerHTML = "Select Help menu item";
+    }
+}
+
+function resetToolTip(e){
+    let tooltip = document.getElementById("tooltip");
+    tooltip.innerHTML = "Tooltips go here (^_^)";
+}
+
+let menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(item => item.addEventListener("mouseover", setToolTip));
+menuItems.forEach(item => item.addEventListener("mouseleave", resetToolTip));
